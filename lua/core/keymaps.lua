d8bbee9bff3dbecdb0646da198baca02bf63473b
Jared Perkins <jared.perkins@hotmail.com>
2025-11-10 09:29:13 -0500

    Configured telescope with keybindings.

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,7 +1,9 @@
--- Set leader key before anything else
+-- leader key before anything else
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
 -- Key bindings for nvim-tree file browser
 vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file browser" })
 vim.keymap.set("n", "<leader>f", "<cmd>NvimTreeFocus<CR>", { desc = "Focus file browser" })
+
+
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -89,7 +89,37 @@ require("lazy").setup({
       },
     })
   end,
+},
+{
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.5", -- or latest stable
+  dependencies = { "nvim-lua/plenary.nvim" },
+    keys = {
+        { "<leader>tf", function() require("telescope.builtin").find_files() end, desc = "Telescope: Find files" },
+        { "<leader>tg", function() require("telescope.builtin").live_grep() end, desc = "Telescope: Live grep" },
+        { "<leader>tb", function() require("telescope.builtin").buffers() end, desc = "Telescope: Buffers" },
+        { "<leader>th", function() require("telescope.builtin").help_tags() end, desc = "Telescope: Help tags" },
+    },
+  config = function()
+    require("telescope").setup({
+      defaults = {
+        layout_strategy = "horizontal",
+        layout_config = {
+          prompt_position = "top",
+          preview_width = 0.6,
+        },
+        sorting_strategy = "ascending",
+        winblend = 10,
+      },
+      pickers = {
+        find_files = {
+          hidden = true,
+        },
+      },
+    })
+  end,
 }
+
 
 
   -- Add more plugins here
