9fc4e623493aed32d367e548708929bcbea71ca1
Jared Perkins <jared.perkins@hotmail.com>
2025-11-10 08:10:03 -0500

    Setup lsp. Gives a warning about deprecation. Have to live with it for now.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -11,7 +11,21 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
     { "nvim-lualine/lualine.nvim" },
     { "nvim-tree/nvim-web-devicons" },
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    {
+  "neovim/nvim-lspconfig", -- Core LSP support
+},
+{
+  "williamboman/mason.nvim",       -- LSP/DAP installer
+  "williamboman/mason-lspconfig.nvim", -- Bridge to lspconfig
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup({
+      ensure_installed = { "lua_ls", "pyright", "tsserver" }, -- Add your servers here
+    })
+  end,
+}
+
   -- Add more plugins here
 })
 
---
+++ lua/user/lsp.lua
@@ -0,0 +1,21 @@
+local lspconfig = require("lspconfig")
+
+-- Optional: Add capabilities for completion plugins
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+-- capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities) -- if using nvim-cmp
+
+-- Define servers and their settings
+local servers = {
+  lua_ls = {},
+  pyright = {},
+  tsserver = {},
+}
+
+-- Loop through and setup each server
+for server, config in pairs(servers) do
+  lspconfig[server].setup({
+    capabilities = capabilities,
+    settings = config,
+  })
+end
+
