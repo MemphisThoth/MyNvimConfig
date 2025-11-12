2e96255eb32cc6c4f0eb06549ab97201d09ccadc
Jared Perkins <jared.perkins@hotmail.com>
2025-11-12 13:40:23 -0500

    Configured grug-far.nvim to use search and replace.

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -283,6 +283,28 @@ require("lazy").setup({
     'akinsho/bufferline.nvim', 
         version = "*", 
         dependencies = 'nvim-tree/nvim-web-devicons'
+},
+{
+  "MagicDuck/grug-far.nvim",
+  opts = { headerMaxWidth = 80 },
+  cmd = { "GrugFar", "GrugFarWithin" },
+  keys = {
+    {
+      "<leader>sr",
+      function()
+        local grug = require("grug-far")
+        local ext = vim.bo.buftype == "" and vim.fn.expand("%:e")
+        grug.open({
+          transient = true,
+          prefills = {
+            filesFilter = ext and ext ~= "" and "*." .. ext or nil,
+          },
+        })
+      end,
+      mode = { "n", "x" },
+      desc = "Search and Replace",
+    },
+  },
 }
 
 
